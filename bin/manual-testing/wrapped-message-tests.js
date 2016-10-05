51e230f243566a3bb0e1cb57afd3c622a2dd7715
Lucas Nagle <lnagle@gmail.com>
2016-10-05 08:18:12 -0500

    Add testPrep method and refactor for DRYness

--- bin/manual-testing/wrapped-message-tests.js
+++ bin/manual-testing/wrapped-message-tests.js
@@ -5,6 +5,26 @@ var config, Plugin;
 config = require("./manualTestConfig");
 Plugin = require("../../lib/index")(config);
 
+
+/**
+ * Disconnects from the database sometime after the timeout.
+ *
+ * @param {ddqPlugin~instance} instance
+ */
+function handleDisconnect(instance) {
+    setTimeout(() => {
+        instance.disconnect((err) => {
+            if (err) {
+                console.log("There was a problem disconnecting");
+                throw new Error(err);
+            } else {
+                console.log("Disconnected successfully");
+            }
+        });
+    }, 2000);
+}
+
+
 /**
  * Instantiates a plugin and uses its connection to clear all records from the
  * database.
@@ -31,48 +51,18 @@ function cleanup() {
                 console.log(data);
             });
         }
-        setTimeout(() => {
-            instance.disconnect((err) => {
-                if (err) {
-                    console.log("There was a problem disconnecting", err);
-                } else {
-                    console.log("Disconnected successfully");
-                }
-            });
-        }, 2000);
+
+        handleDisconnect(instance);
     });
 }
 
 
 /**
- * Wrapper for testing the individual methods that are received from a DDQ
- * Plugin wrapped message.
+ * Connects to the database and calls checkNow.
  *
- * @param {Function} fn
+ * @param {ddqPlugin~instance} instance
  */
-function wrappedMessageTest(fn) {
-    var instance;
-
-    instance = new Plugin();
-
-    instance.on("data", (data) => {
-        console.log("CheckNow data listener activated");
-        console.log("data", data);
-        data[fn]((err, fnData) => {
-            if (err) {
-                console.log("There was an error");
-                throw new Error(err);
-            } else {
-                console.log("Function was successful");
-                console.log("Data:", fnData);
-            }
-        });
-    });
-
-    instance.on("error", (err) => {
-        console.log("CheckNow error listener activated");
-        throw new Error(err);
-    });
+function handleConnection(instance) {
     instance.connect((connectErr) => {
         if (connectErr) {
             console.log("There was a connection error");
@@ -81,60 +71,55 @@ function wrappedMessageTest(fn) {
 
         console.log("Connection was successfully made");
         instance.checkNow();
-
-        setTimeout(() => {
-            instance.disconnect((err) => {
-                if (err) {
-                    console.log("There was a problem disconnecting");
-                    throw new Error(err);
-                } else {
-                    console.log("Disconnected successfully");
-                }
-            });
-        }, 2000);
+        handleDisconnect(instance);
     });
 }
 
 
 /**
- * Creates a record that has a hash, an isProcessing value of true, and an owner
- * value that is the same as that set to the config and adds that record to the
- * database. In short, it is a match for the conditions that heartbeat searches
- * for.
+ * Preps and runs individual tests for wrapped message methods.
+ *
+ * @param {Function} fn The function being tested.
+ * @param {string} query The query to prep for the function.
  */
-function heartbeatPrep() {
+function wrappedMessageTest(fn, query) {
     var instance;
 
     instance = new Plugin();
+
+    instance.on("data", (data) => {
+        console.log("CheckNow data listener activated");
+        // Preps for the next function call to find a record.
+        instance.connection.query(query, [], (prepErr) => {
+            if (prepErr) {
+                console.log(`There was an error while prepping the ${fn} test`);
+                throw new Error(prepErr);
+            }
+
+            data[fn]((err, fnData) => {
+                if (err) {
+                    console.log("There was an error");
+                    throw new Error(err);
+                } else {
+                    console.log("Function was successful");
+                    console.log("Data:", fnData);
+                }
+            });
+        });
+    });
+
+    instance.on("error", (err) => {
+        console.log("CheckNow error listener activated");
+        throw new Error(err);
+    });
+    handleConnection(instance);
 }
 
-/**
- * Creates a record and adds it to the database.
- */
-function requeuePrep() {
-    var instance;
 
-    instance = new Plugin();
-}
-
-/**
- * Creates a record that has a hash and a requeued value set to false and adds
- * it to the database.
- */
-function removePrep() {
-    var instance;
-
-    instance = new Plugin();
-}
-
-
-heartbeatPrep();
-wrappedMessageTest("heartbeat");
+wrappedMessageTest("heartbeat", "INSERT INTO instance.config.table SET hash = data.recordId, isProcessing = true, owner = instance.owner;");
 cleanup();
-requeuePrep();
-wrappedMessageTest("requeue");
+wrappedMessageTest("requeue", "INSERT INTO instance.config.table SET hash = data.recordId;");
 cleanup();
-removePrep();
-wrappedMessageTest("remove");
+wrappedMessageTest("remove", "INSERT INTO instance.config.table SET hash = data.recordId, requeued = false;");
 cleanup();
 
